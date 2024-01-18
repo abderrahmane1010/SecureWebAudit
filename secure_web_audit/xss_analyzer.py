b603b8696f759ee650d46ff84142689f510f6926
abderrahmane <bougabderrahmane@gmail.com>
2024-01-18 23:38:20 +0100

    add xss code or script

--- secure_web_audit/xss_analyzer.py
+++ secure_web_audit/xss_analyzer.py
@@ -9,11 +9,12 @@ class XSSAnalyzer :
 
     def __init__(self, url):
         self.url = url 
-        self.find_user_inputs()
-        banner_xss(url)
+        # banner_xss(url)
+        self.find_possible_injection()
         
-    def find_user_inputs(self):
-        xss_test_script = "testforxssplease"
+        
+    def find_possible_injection(self):
+        xss_test_script = "texthuihaobzaoibzhauihbzuiahbizha"
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, 'html.parser')
         
@@ -31,20 +32,41 @@ class XSSAnalyzer :
             elif method == "get":
                 response1 = requests.get(form_url, params=data)
             
+            html_content = BeautifulSoup(response1.text, 'html.parser')
+            
             if xss_test_script in response1.text:
-                print(colorize(f"Possible XSS vulnerability detected at {form_url}","error"))
-
-            # soup1 = BeautifulSoup(response1.text, 'html.parser')
-
-            # print(f"Response to {method.upper()} request to {form_url}: {response1}")
+                print(colorize(f" [Form] Possible XSS vulnerability detected at {form_url}","error"))
+                mot = html_content.find(string=xss_test_script)
+                if mot :
+                    parent = mot.find_parent()
+                    if parent :
+                        print(colorize(f'HTML CODE : {parent.find_parent()}',"magenta"))
+                    else :
+                        print(colorize(f'HTML CODE : {parent}',"magenta"))
+                script_tag = html_content.find_all('script')
+                for s in script_tag :
+                    if xss_test_script in s.text :
+                        print(colorize(f'SCRIPT : {s.prettify()}',"magenta"))
             
         parsed_url = urlparse(self.url)
         params = parse_qs(parsed_url.query)
         if len(params) != 0 :
             data = {}
             for param in params:
-                # print(f"URL parameter found: {param}")
                 data[param] = xss_test_script
-            response1 = requests.get(self.url, params=data)
-            if xss_test_script in response1.text:
-                print(colorize(f"Possible XSS vulnerability detected at {self.url}","error"))
+            response2 = requests.get(self.url, params=data)
+            html_content1 = BeautifulSoup(response2.text, 'html.parser')
+            if xss_test_script in response2.text:
+                print(colorize(f" [Params] Possible XSS vulnerability detected at {self.url}","error"))
+                mot = html_content1.find(string=xss_test_script)
+                if mot :
+                    parent = mot.find_parent()
+                    if parent :
+                        print(colorize(f'HTML CODE : {parent.find_parent()}',"magenta"))
+                    else :
+                        print(colorize(f'HTML CODE : {parent}',"magenta"))
+                        
+                script_tag1 = html_content1.find_all('script')
+                for s in script_tag1 :
+                    if xss_test_script in s.text :
+                        print(colorize(f'SCRIPT : {s.prettify()}',"magenta"))
